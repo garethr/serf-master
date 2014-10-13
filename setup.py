29908994fb90bae980a43f8be6c58955d8adca03
Gareth Rushgrove <gareth@morethanseven.net>
2014-10-13 08:50:16 +0100

    fix issue #8, include files in source package

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import multiprocessing
 from setuptools import setup
 
 setup(name='serf_master',
-    version='0.3',
+    version='0.4',
     description='helpers for writing manageable Serf handlers',
     classifiers=[
         'Development Status :: 3 - Alpha',
